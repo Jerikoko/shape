d01a1d9e569be550f07854bb4e1d26fe14395cf5
Jessica Marina Jacinto <W7348149@student.miracosta.edu>
2021-09-06 21:56:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿/// Chapter No. 1		Exercise No. 1
+/// File Name: shape.java
+/// @author: Jeriko Jacointo
+/// Date: 6 September 2021
+///
+/// Problem Statement: Create a simple shape using WriteLine statements
+/// 
+/// 
+/// Overall Plan:
+/// 1) Print 2 horizontal lines
+/// 2) Print two vertical lines
+/// 
+using System;
+
+namespace shape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("______________s");
+            Console.WriteLine("|            |\n|            |\n|            |\n|            |\n|            |\n|            |\n|            |");
+            Console.WriteLine("|____________|");
+
+
+        }
+    }
+}
